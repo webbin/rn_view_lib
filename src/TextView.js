05b6dd86fa6fdaab2bb51bbf8494e93f4cf738bd
zhengWeiBin <1358102877@qq.com>
2020-03-24 16:20:17 +0800

    update

--- src/TextView.js
+++ src/TextView.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-03-24 10:14:53
- * @LastEditTime: 2020-03-24 11:17:48
+ * @LastEditTime: 2020-03-24 14:46:44
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rn_view_lib/src/TextView.js
@@ -22,7 +22,7 @@ export default class TextView extends Component {
         style={{
           marginVertical: 20,
         }}>
-        <Text>version 1003</Text>
+        <Text>version 0.0.2</Text>
         <Text style={{fontSize: 20, color: 'black'}}> TextView </Text>
       </View>
     );
