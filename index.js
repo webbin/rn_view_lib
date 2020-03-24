3bced26b63fbbe5445a19f9ae8de8d5c416fb2fb
zhengWeiBin <1358102877@qq.com>
2020-03-24 10:21:10 +0800

    init project

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+// AppRegistry.registerComponent(appName, () => App);
+
+import TextView from './src/TextView';
+
+module.exports = {
+  TextView,
+};
---
+++ src/TextView.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-24 10:14:53
+ * @LastEditTime: 2020-03-24 10:18:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /rn_view_lib/src/TextView.js
+ */
+
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class TextView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          marginVertical: 20,
+        }}>
+        <Text> TextView </Text>
+      </View>
+    );
+  }
+}
